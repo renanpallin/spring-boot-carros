324c3fa6d94de6118644c8f9aa49d25f4b98af55
Renan Pallin <renanpallin@gmail.com>
2019-12-31 03:09:25 -0300

    Testes with JUnit

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+    @Autowired
+    private CarroService service;
+
     @Test
-    void contextLoads() {
+    public void testSalvar() {
+        Carro c = new Carro();
+        c.setNome("Ferrari");
+        c.setTipo("esportivos");
+
+        Carro inserted = service.insert(c);
+        assertNotNull(inserted);
+
+        Long id = c.getId();
+        assertNotNull(id);
+
+        Optional<Carro> opCarroRecuperado = service.getById(id);
+        assertTrue(opCarroRecuperado.isPresent());
+        Carro carroRecuperado = opCarroRecuperado.get();
+        assertEquals("Ferrari", carroRecuperado.getNome());
+        assertEquals("esportivos", carroRecuperado.getTipo());
+    }
+
+    @Test
+    public void testLista() {
+        assertEquals(30,
+                service.getCarros().size());
+
     }
 
 }
