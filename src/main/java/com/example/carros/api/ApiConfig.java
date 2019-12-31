0e7a837e2315868c19005d60363ec6088331ec81
Renan Pallin <renanpallin@gmail.com>
2019-12-31 02:42:38 -0300

    Add prefix to URL with configurePathMatch on ApiConfig

---
+++ src/main/java/com/example/carros/api/ApiConfig.java
@@ -0,0 +1,17 @@
+package com.example.carros.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ApiConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api/v1", HandlerTypePredicate.forAnnotation(RequestMapping.class));
+    }
+}
--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/carros")
+@RequestMapping("/carros")
 public class CarrosController {
 
     @Autowired
